6ca6642e89a33c0fe78bc5baf4af5d0b21d9afcb
U-BSD\398256 <398256@SHS-DGDRCS2.beaverton.k12.or.us>
2023-02-28 14:30:08 -0800

    working

--- heap.cpp
+++ heap.cpp
@@ -2,23 +2,24 @@
 #include <iostream>
 #include <cmath>
 #include <cstring>
+#include <vector>
 
 using namespace std;
 
-void add(int number, int index, int (&heap)[100]);
-void swapNums(int &p, int &c, int (&heap)[100]);
-void print(int (&hash)[100]);
+void add(int number, int index, int (&heap)[102]);
+void swapNums(int &p, int &c, int (&heap)[102]);
+void print(int (&hash)[102], int current, int depth);
 
 
 int main(){
-  int heap[100]; //initial my heap
+  int heap[102]; //initial my heap
   char command[10];
-  int index;
-  bool running;
+  int index = 0;
+  bool running = true;
 
   
   //initialize all heap values to NULL:
-  for(int i = 0; i < 100; i++){
+  for(int i = 0; i < 102; i++){
     heap[i] = NULL;
   }
 
@@ -39,13 +40,34 @@ int main(){
 	//iterate through vector and add to heap
 	char fileName[50];
 	cout << "What is the name of the file?" << endl;
-	cin.get(file, 50);
+	cin.get(fileName, 50);
 	cin.ignore(50, '\n');
 	
 	vector<int> fileNums;
-	ifstream file(fileName);
 	
-      
+	ifstream file(fileName);
+	if(file.is_open()){
+	  int a;
+	  while(file >> a){
+	    fileNums.push_back(a);
+	  }
+	  file.close();
+	}else{
+	  cout << "File could not be opened!" << endl;
+	}
+
+	vector<int>::iterator ptr;
+	for(ptr = fileNums.begin(); ptr < fileNums.end(); ptr++){
+	  for(int i = 0; i < 102; i++){
+	    if(heap[i] == NULL &&
+	       i != 0){
+	      index = i;
+	      break;
+	    }
+	  }
+	  add((*ptr), index, heap);
+	}
+
       }else if(strcmp(command, "NUMBER") == 0){ //adding number
 	int number;
 	cout << "Enter a number:" << endl;
@@ -53,7 +75,7 @@ int main(){
 	cin.ignore(10, '\n');
 		
 	//find index number:
-	for(int i = 0; i < 100; i++){//find next open spot
+	for(int i = 0; i < 102; i++){//find next open spot
 	  if(heap[i] == NULL &&
 	     i != 0){
 	    index = i; //don't include 0
@@ -64,22 +86,22 @@ int main(){
 	add(number, index, heap);
       }
     }
-    if(heap[100] != NULL){//all slots filled
-      print(heap);
+   
+    if(heap[102] != NULL){//all slots filled
+      print(heap, 1, 0);
     }
   }
   return 0;
 }
 
 
-void add(int number, int index, int (&heap)[100]){
+void add(int number, int index, int (&heap)[102]){
   //add number to the next open index
   //check its parent
   //if greater, swap
   //if less done
   heap[index] = number;
   int parIndex = floor(index/2);
-  cout << "parIndex: " << parIndex << endl;
 
   while(heap[parIndex] < number &&
 	parIndex != 0){ //parent is less - must be swapped
@@ -88,7 +110,7 @@ void add(int number, int index, int (&heap)[100]){
   
 }
 
-void swapNums(int &p, int &c, int (&heap)[100]){
+void swapNums(int &p, int &c, int (&heap)[102]){
   //switch the parent and child
   int hold;
   hold = heap[p];
@@ -98,9 +120,19 @@ void swapNums(int &p, int &c, int (&heap)[100]){
 
 
 
-void print(int (&hash)[100]){
-  for(int i = 0; i < 100; i++){
-    cout << hash[i] << endl;
+
+
+void print(int (&hash)[102], int current, int depth){
+  if((current*2 +1) != NULL){
+    print(hash, (current*2 +1), (depth + 1));
+  }
+  for(int i = 0; i < depth; i++){
+    cout << '\t';
+  }
+  cout << current << endl;
+
+  if((current*2) != NULL){
+    print(hash, (current*2), (depth +1));
   }
 }
 
